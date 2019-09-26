91f1fe46ff1cca074bd1278fd88a6250fe51344f
shonjiho <jihoson94@gmail.com>
2019-09-26 15:48:14 +0900

    add User Models data field (birthdate, lanaguage, currency, superhost)

--- users/models.py
+++ users/models.py
@@ -20,3 +20,21 @@ class User(AbstractUser):
         choices=GENDER_CHOICES, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="")
+
+    LANGUAGE_ENGLISH = "english"
+    LANGUAGE_KOREA = "korean"
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREA, "Korea"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
