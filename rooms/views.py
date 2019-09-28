aede81670d212d7877e498075d0c1d71ad7b5dc2
shonjiho <jihoson94@gmail.com>
2019-09-28 20:13:08 +0900

    add page handling Emptypaging

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)  # Paginator(<item_list>, page_size)
-    rooms = paginator.get_page(page)
-    # rooms = paginator.page(page)
-    return render(request, "rooms/home.html", {"page": rooms})
+    # rooms = paginator.get_page(page)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except Exception:
+        return redirect("/")
