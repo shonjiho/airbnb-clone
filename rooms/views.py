fae903788d48fbf250f948b992929d5aa0e9dc68
shonjiho <jihoson94@gmail.com>
2019-09-30 15:28:16 +0900

    add 404.html at HTTP404 ERROR

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView
 from django.utils import timezone
 from django.shortcuts import render, redirect
+from django.http import Http404
 from django.urls import reverse
 from . import models as room_models
 
@@ -26,4 +27,5 @@ def room_detail(request, pk):
         room = room_models.Room.objects.get(pk=pk)
         return render(request, "rooms/detail.html", context={"pk": pk, "room": room})
     except room_models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+        # On DEBUG == False, Http404() show templates/404.html -> this is default
+        raise Http404()
