8eadc8e65cb6e5b217ae2c54ab9cf249b37d00de
shonjiho <jihoson94@gmail.com>
2019-09-30 17:48:31 +0900

    for select tag, modify rooms.views.search and search.html

--- rooms/views.py
+++ rooms/views.py
@@ -34,10 +34,13 @@ class RoomDetail(DetailView):
 def search(request):
     city = request.GET.get("city", "AnyWhere")
     city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
     room_types = room_models.RoomType.objects.all()
-    return render(
-        request,
-        "rooms/search.html",
-        {"city": city, "countries": countries, "room_types": room_types},
-    )
+
+    form = {"city": city, "s_country": country, "s_room_type": room_type}
+
+    choices = {"countries": countries, "room_types": room_types}
+
+    return render(request, "rooms/search.html", {**form, **choices})
 
