41d38267701654a67edc75d6c906232569239636
shonjiho <jihoson94@gmail.com>
2019-11-03 12:54:54 +0900

    add login feature

--- users/views.py
+++ users/views.py
@@ -1,15 +1,23 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login
 from . import forms
 
 
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm(initial={"email": "jisho@naver.com"})
+        form = forms.LoginForm(initial={"email": "jihoson94@naver.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
+
         return render(request, "users/login.html", {"form": form})
