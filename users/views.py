f83a6a47736ff0e317d4f4a9c57d3312633a74cc
shonjiho <jihoson94@gmail.com>
2019-11-03 17:21:16 +0900

    login view change generic view

--- users/views.py
+++ users/views.py
@@ -1,26 +1,26 @@
-from django.views import View
-from django.shortcuts import render, redirect, reverse
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, reverse
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm(initial={"email": "jihoson94@naver.com"})
-        return render(request, "users/login.html", {"form": form})
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "email": "jihoson94@gmail.com"
+    }
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
 
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-
-        return render(request, "users/login.html", {"form": form})
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
 
 def log_out(request):
