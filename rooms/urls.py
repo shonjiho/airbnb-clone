4a75a85f38ab8801d4d5e4d14c155000b2b729af
shonjiho <jihoson94@gmail.com>
2020-02-07 14:42:20 +0900

    add EditRoomView

--- rooms/urls.py
+++ rooms/urls.py
@@ -5,5 +5,6 @@ app_name = "rooms"
 
 urlpatterns = [
     path("<int:pk>/", room_views.RoomDetail.as_view(), name="detail"),
+    path("<int:pk>/edit", room_views.EditRoomView.as_view(), name="edit"),
     path("search/", room_views.RoomSearch.as_view(), name="search"),
 ]
