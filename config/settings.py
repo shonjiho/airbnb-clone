0ae30754e00bffc0cdcd9ce24055a274618a3b50
shonjiho <jihoson94@gmail.com>
2020-02-12 22:55:26 +0900

    AWS S3 Cache settings

--- config/settings.py
+++ config/settings.py
@@ -175,6 +175,7 @@ if not DEBUG:
     AWS_AUTO_CREATE_BUCKET = True
     AWS_BUCKET_ACL = 'public-read'
     AWS_DEFAULT_ACL="public-read"
+    AWS_S3_OBJECt_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
