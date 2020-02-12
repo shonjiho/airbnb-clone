787115fa5ae26deef0a8f191a891d35ebc04b3b2
shonjiho <jihoson94@gmail.com>
2020-02-12 15:59:39 +0900

    deploy settings

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "h++hoici3iu*@kw^c3cc*gpzv=62i0duv#l^qj3m!(t8=ne!y3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = ['airbnb-clone.dq3k7wxkbi.ap-northeast-2.elasticbeanstalk.com']
+    
 
 
 # Application definition
@@ -88,13 +92,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE":"django.db.backends.postgresql_psycopg2",
+            "HOST":os.environ.get("RDS_HOST"),
+            "NAME":os.environ.get("RDS_NAME"),
+            "USER":os.environ.get("RDS_USER"),
+            "PASSWORD":os.environ.get("RDS_PASSWORD"),
+            "PORT":os.environ.get("RDS_PORT"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
