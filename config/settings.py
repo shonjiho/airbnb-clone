ac536c65108264e6326206d9099350abfdd7e6e4
shonjiho <jihoson94@gmail.com>
2020-02-12 16:04:48 +0900

    edit && eb deploy

--- config/settings.py
+++ config/settings.py
@@ -23,12 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "h++hoici3iu*@kw^c3cc*gpzv=62i0duv#l^qj3m!(t8=ne!y3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG'))
+# DEBUG = bool(os.environ.get('DEBUG'))
+DEBUG = True
 
-if DEBUG:
-    ALLOWED_HOSTS = []
-else:
-    ALLOWED_HOSTS = ['airbnb-clone.dq3k7wxkbi.ap-northeast-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
     
 
 
@@ -92,7 +90,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG:
+if DEBUG is False:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
